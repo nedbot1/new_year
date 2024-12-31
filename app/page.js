2f13f75bb42f0eb5807dfe0ed3c1a453bc8eb5bd
tobden <tobdheng@gmail.com>
2024-12-31 16:23:50 +0600

    first commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,130 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  useEffect(() => {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    // Set Canvas to Full Screen
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    // Resize Canvas when window resizes
+    window.addEventListener("resize", () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    });
+
+    // Rockets and Sparkles arrays
+    let fireRocketsArray = [];
+    let fireRocketsSparklesArray = [];
+
+    function FireRockets() {
+      this.x = Math.floor(Math.random() * window.innerWidth);
+      this.y = window.innerHeight;
+      this.color = `hsl(${Math.floor(Math.random() * 360)},70%,50%)`;
+      this.size = Math.floor(Math.random() * 5 + 5);
+      this.speedY = Math.random() * 5 + 5;
+      this.crackRocketY = Math.floor(
+        window.innerHeight - (Math.random() * window.innerHeight + 100)
+      );
+
+      this.update = () => {
+        this.y -= this.speedY;
+      };
+
+      this.draw = () => {
+        context.fillStyle = this.color;
+        context.beginPath();
+        context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        context.fill();
+      };
+    }
+
+    function FireRocketsSparkles(x, y, color) {
+      this.x = x;
+      this.y = y;
+      this.color = color;
+      this.size = Math.floor(Math.random() * 3 + 6);
+      this.speedY = Math.random() * 2 - 2;
+      this.speedX = Math.round((Math.random() - 0.5) * 10);
+      this.velocity = Math.random() / 5;
+
+      this.update = () => {
+        if (this.size > 0.2) {
+          this.size -= 0.1;
+        }
+        this.y += this.speedY;
+        this.x += this.speedX;
+        this.speedY += this.velocity;
+      };
+
+      this.draw = () => {
+        context.fillStyle = this.color;
+        context.beginPath();
+        context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        context.fill();
+      };
+    }
+
+    function renderFireRockets() {
+      for (let i = 0; i < fireRocketsArray.length; i++) {
+        fireRocketsArray[i].draw();
+        fireRocketsArray[i].update();
+        if (fireRocketsArray[i].y <= fireRocketsArray[i].crackRocketY) {
+          for (let index = 0; index < 20; index++) {
+            fireRocketsSparklesArray.push(
+              new FireRocketsSparkles(
+                fireRocketsArray[i].x,
+                fireRocketsArray[i].y,
+                fireRocketsArray[i].color
+              )
+            );
+          }
+          fireRocketsArray.splice(i, 1);
+          i--;
+        }
+      }
+    }
+
+    function renderFireRocketsSparkles() {
+      for (let i = 0; i < fireRocketsSparklesArray.length; i++) {
+        fireRocketsSparklesArray[i].draw();
+        fireRocketsSparklesArray[i].update();
+        if (fireRocketsSparklesArray[i].size <= 0.2) {
+          fireRocketsSparklesArray.splice(i, 1);
+          i--;
+        }
+      }
+    }
+
+    function animate() {
+      context.fillStyle = `rgba(24,28,31,.2)`;
+      context.fillRect(0, 0, canvas.width, canvas.height);
+      context.fillStyle = `white`;
+      renderFireRockets();
+      renderFireRocketsSparkles();
+      requestAnimationFrame(animate);
+    }
+
+    animate();
+
+    setInterval(() => {
+      for (let i = 0; i < 4; i++) {
+        fireRocketsArray.push(new FireRockets());
+      }
+    }, 600);
+  }, []);
+
+  return (
+    <div>
+      <canvas id="canvas" className="absolute inset-0"></canvas>
+      <div className="heading flex flex-col items-center justify-center bg-slate-500/55 rounded-3xl p-10 text-center">
+        <h1>HAPPY NEW YEAR!!!</h1>
+        <span>2025</span>
+      </div>
     </div>
   );
 }
